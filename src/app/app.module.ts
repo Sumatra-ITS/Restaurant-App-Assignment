ce94b41374ca2369faed73c3906efebe684ec856
abdelmonaim <abdelmonaim.serrhini@gmail.com>
2024-07-05 12:05:58 +0100

    Add tailwind css and primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ButtonModule} from "primeng/button";
+import {SidebarModule} from "primeng/sidebar";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { SelectButtonComponent } from './components/select-button/select-button.component';
+import { SearchInputComponent } from './components/search-input/search-input.component';
+import { DishCardComponent } from './components/dish-card/dish-card.component';
+import { RestaurantCoverComponent } from './components/restaurant-cover/restaurant-cover.component';
+import {NgOptimizedImage} from "@angular/common";
+import { H2Component } from './components/h2/h2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectButtonComponent,
+    SearchInputComponent,
+    DishCardComponent,
+    RestaurantCoverComponent,
+    H2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SidebarModule,
+    ButtonModule,
+    NgOptimizedImage,
   ],
   providers: [
     provideClientHydration()
