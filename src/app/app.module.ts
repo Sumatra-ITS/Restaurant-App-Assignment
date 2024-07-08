8621d9116bde2cf3ccc4a8d796c1f8ed0b737edd
abdelmonaim <abdelmonaim.serrhini@gmail.com>
2024-07-08 10:32:01 +0100

    Remove some unused modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {ButtonModule} from "primeng/button";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {SidebarModule} from "primeng/sidebar";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import { SelectButtonComponent } from './components/select-button/select-button.component';
-import { SearchInputComponent } from './components/search-input/search-input.component';
-import { DishCardComponent } from './components/dish-card/dish-card.component';
-import { RestaurantCoverComponent } from './components/restaurant-cover/restaurant-cover.component';
+import {SelectButtonComponent} from './components/select-button/select-button.component';
+import {SearchInputComponent} from './components/search-input/search-input.component';
+import {DishCardComponent} from './components/dish-card/dish-card.component';
+import {RestaurantCoverComponent} from './components/restaurant-cover/restaurant-cover.component';
 import {NgOptimizedImage} from "@angular/common";
-import { H2Component } from './components/h2/h2.component';
-import {ToggleButtonModule} from "primeng/togglebutton";
-import { ButtonComponent } from './components/button/button.component';
-import { H4Component } from './components/h4/h4.component';
-import { PComponent } from './components/p/p.component';
+import {H2Component} from './components/h2/h2.component';
+import {ButtonComponent} from './components/button/button.component';
+import {H4Component} from './components/h4/h4.component';
+import {PComponent} from './components/p/p.component';
 import {BadgeModule} from "primeng/badge";
-import { CounterComponent } from './components/counter/counter.component';
-import { IconButtonComponent } from './components/icon-button/icon-button.component';
+import {CounterComponent} from './components/counter/counter.component';
+import {IconButtonComponent} from './components/icon-button/icon-button.component';
 import {provideHttpClient, withFetch} from "@angular/common/http";
-import { OrderComponent } from './components/order/order.component';
-import { PaymentComponent } from './components/payment/payment.component';
-import { H3Component } from './components/h3/h3.component';
-import { OrderLineComponent } from './components/order-line/order-line.component';
-import { DataListComponent } from './components/data-list/data-list.component';
-import { H5Component } from './components/h5/h5.component';
-import { InputFieldComponent } from './components/input-field/input-field.component';
-import { CheckboxComponent } from './components/checkbox/checkbox.component';
+import {OrderComponent} from './components/order/order.component';
+import {PaymentComponent} from './components/payment/payment.component';
+import {H3Component} from './components/h3/h3.component';
+import {OrderLineComponent} from './components/order-line/order-line.component';
+import {DataListComponent} from './components/data-list/data-list.component';
+import {H5Component} from './components/h5/h5.component';
+import {InputFieldComponent} from './components/input-field/input-field.component';
+import {CheckboxComponent} from './components/checkbox/checkbox.component';
 
 @NgModule({
   declarations: [
@@ -56,9 +54,7 @@ import { CheckboxComponent } from './components/checkbox/checkbox.component';
     BrowserAnimationsModule,
     AppRoutingModule,
     SidebarModule,
-    ButtonModule,
     NgOptimizedImage,
-    ToggleButtonModule,
     BadgeModule,
   ],
   providers: [
@@ -67,4 +63,5 @@ import { CheckboxComponent } from './components/checkbox/checkbox.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
