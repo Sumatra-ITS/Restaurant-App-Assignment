0f210d4b3ec38f73b513b76be27518ca5182822c
abdelmonaim <abdelmonaim.serrhini@gmail.com>
2024-07-08 11:47:58 +0100

    Make it responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,6 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    screens: {
-      minmd: { min: '768px' },
-      minlg: { min: '1024px' },
-      minxl: { min: '1280px' },
-      min2xl: { min: '1116px' },
-    },
   },
   extend: {},
   plugins: [],
