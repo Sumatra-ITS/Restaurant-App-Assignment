d06df0c20619d49f7fd31b6de86c8898b9586b01
abdelmonaim <abdelmonaim.serrhini@gmail.com>
2024-07-10 11:21:56 +0100

    Use the correct colors, define global colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    extend: {
+      colors: {
+        bl: '#12131A',
+        'gr-2': '#F7F7F7',
+        'gr-3': '#EBE7E7',
+        'gr-4': '#9399A1',
+        og: '#F64E0C'
+      }
+    }
   },
   extend: {},
   plugins: [],
